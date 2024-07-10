26c86cb80b4892a35a7cadec66f78231053526ba
archeskeith <kmarches@up.edu.ph>
2024-07-10 22:52:29 +0800

    added packages.txt

--- st-app.py
+++ st-app.py
@@ -1,8 +1,9 @@
 import streamlit as st
 from PIL import Image
-import fitz
+# import fitz
 # from fitz import *
 import pdfplumber
+
 # from fitz_fix.fitz import * 
 from PyPDF2 import PdfMerger
 from concurrent.futures import ThreadPoolExecutor
